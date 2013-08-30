d372ffcc10171985956800047b8d501ed0dd3b1b
Peter Waller <peter@scraperwiki.com>
2013-08-30 15:28:36 +0100

    Improvements to CA certificate propagation and container startup

--- init.sh
+++ init.sh
@@ -5,6 +5,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 [ -d cobalt ] || git clone git://github.com/scraperwiki/cobalt
 [ -d custard ] || git clone git://github.com/scraperwiki/custard
 
-
 [ -e ${HOME}/.profile ] && ln -sf ${HOME}/.profile $DIR/profile || touch profile
 [ -e ${HOME}/.bashrc ] && ln -sf ${HOME}/.bashrc $DIR/bashrc || touch bashrc
+
+# TODO: generate keypair for MITMing our container
+
+#openssl genrsa -des3 -out mitm-ca.key 4096 -
+genpkey -out mitm-ca.key -pkeyopt rsa_keygen_bits:4096
+openssl req -new -x509 -days 365 -key mitm-ca.key -out mitm-ca.crt
--- run.sh
+++ run.sh
@@ -1,17 +1,41 @@
 #! /usr/bin/env bash
+
+set -e -u
+
+export DOCKER_HOST_IF="$(ip address show docker0 | sed -r -n 's|^.*inet (.*)/.*$|\1|gp')"
+
+
+# TODO: Later versions of NPM have a list of CAs..
+#NPM_CA="$(npm config --global get ca | sed "s| ]|, '$(sed ':a;N;$!ba;s/\n/\\\\n/g' ./mitm-ca.crt)' ]|g")"
+
+NPM_CA="$(sed ':a;N;$!ba;s/\n/\\\\\\\\n/g' ./mitm-ca.crt)"
+
+# TODO: transparent proxy hijacking
+
+
 eval echo \""$(cat Dockerfile.in)"\" > Dockerfile
 
-docker build -q -t sw .
+# transparent proxy for all docker containers
+#sudo iptables -t nat -A PREROUTING -i docker0 -p tcp --dport 80 -j REDIRECT --to-port 3128
+
+time docker build -t sw .
 
 RUNARGS="-h scraperwiki-$(hostname) "
 #RUNARGS="$RUNARGS -v ./custard:/sw/custard "
 #RUNARGS="$RUNARGS -v ./cobalt:/sw/cobalt"
 
-ID=$(docker run $RUNARGS -d sw)
+ID="$(docker run $RUNARGS -d sw)"
 
-ssh -p 2222 localhost
+trap cleanup EXIT
 
-IM=$(docker stop $ID)
+function cleanup() {
+	IM="$(docker stop $ID)"
 
-echo Docker image: $IM
-#echo Docker image: $(docker rm $IM)
+	docker rm $IM
+	echo "Deleted docker image ($IM)"
+}
+
+# HOSTKEY="$(docker run sw cat /etc/ssh/ssh_host_ecdsa_key.pub | cut -d' ' -t1-2)"
+
+# TODO: host key modification
+ssh -oStrictHostKeyChecking=no -p 2222 localhost
